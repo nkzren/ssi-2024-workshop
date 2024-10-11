36e2c087e13388dbf4751c2c3ad274a4e75694d8
nkzren <renan.nakazawa0@gmail.com>
2024-10-11 01:44:06 -0300

    retab docker.sh

--- docker.sh
+++ docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-docker run --rm -d --name workshop-ssi -p 5432:5432 \
-				-e POSTGRES_PASSWORD=banana \
-				-e PGDATA=/var/lib/postgresql/data/pgdata \
-				-v $PWD/dados:/var/lib/postgresql/data \
-				-v $PWD/init_db.sql:/docker-entrypoint-initdb.d/init_db.sql \
-				postgres:16
+docker run --rm -d --name workshop-ssi \
+	-p 5432:5432 \
+	-e POSTGRES_PASSWORD=banana \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	-v $PWD/dados:/var/lib/postgresql/data \
+	-v $PWD/init_db.sql:/docker-entrypoint-initdb.d/init_db.sql \
+	postgres:16
+
